9a586a156449f1144b851c707cbe89b4ece18802
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-02-06 16:46:30 +0300

    Update dawn.sh

--- dawn.sh
+++ dawn.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-git clone https://github.com/Jaammerr/The-Dawn-Bot.git
-cd The-Dawn-Bot
-apt install python3.10-venv -y
-python3 -m venv myenv
-pip install -r requirements.txt
-cd /root/The-Dawn-Bot/config/
-sed -i 's/threads: 100/threads: 1/' settings.yaml
-sed -i 's/use_proxy_for_imap: false/use_proxy_for_imap: true/' settings.yaml
-sed -i 's/two_captcha_api_key: ""/two_captcha_api_key: "ce920aea16dae4a647243305b6cea274"/' settings.yaml
+rm -rf pop && pkill -f pop
+curl -L https://dl.pipecdn.app/v0.2.3/pop -o pop
+chmod +x pop
+/pop --refresh
+./pop
