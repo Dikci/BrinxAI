c42663b3420a62e2df47029ee558a57457ec5c8d
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-02-12 14:20:43 +0300

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf pop && pkill -f pop
-curl -L https://dl.pipecdn.app/v0.2.4/pop -o pop
+curl -L https://dl.pipecdn.app/v0.2.5/pop -o pop
 chmod +x pop
 /pop --refresh
 yes | ./pop
