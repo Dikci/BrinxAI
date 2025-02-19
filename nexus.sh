58a8ad5263db2d581dd6864fcfefb13c6bfc70e9
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-02-19 09:32:57 +0300

    Create nexus.sh

---
+++ nexus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt install -y curl git build-essential pkg-config libssl-dev unzip
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup update
+wget https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protoc-21.12-linux-x86_64.zip
+unzip protoc-21.12-linux-x86_64.zip -d $HOME/.local
+export PATH="$HOME/.local/bin:$PATH"
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+cargo install protobuf-codegen
+rustup target add riscv32i-unknown-none-elf
+rustup component add rust-src
+mkdir -p $HOME/.nexus
+cd $HOME/.nexus
+git clone https://github.com/nexus-xyz/network-api
+cd network-api
+git fetch --tags
+git checkout $(git rev-list --tags --max-count=1)
+cd clients/cli
+cargo clean
+cargo build --release
+rustup target add riscv32i-unknown-none-elf
