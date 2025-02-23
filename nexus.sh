3390a2cea4af257f029a369045555bbfcb7928bf
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2025-02-23 18:46:00 +0300

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
-
-sudo apt install -y curl git build-essential pkg-config libssl-dev unzip
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-rustup update
-export PATH="$HOME/.local/bin:$PATH"
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-cargo install protobuf-codegen
-rustup target add riscv32i-unknown-none-elf
-rustup component add rust-src
-mkdir -p $HOME/.nexus
-cd $HOME/.nexus
-git clone https://github.com/nexus-xyz/network-api
-cd network-api
-git fetch --tags
-git checkout $(git rev-list --tags --max-count=1)
-cd clients/cli
-cargo clean
-cargo build --release
-rustup target add riscv32i-unknown-none-elf
+tmux kill-session -t nexus
+cd && rm -rf .nexus/
+sudo swapoff /swapfile && sudo rm /swapfile && sudo sed -i '/\/swapfile none swap sw 0 0/d' /etc/fstab
